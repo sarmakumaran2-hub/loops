81e4af13a374757fba866fadd84bfbf0aa886b4f
sarma kumaran <sarmakumaran2@gmail.com>
2026-02-12 21:59:36 +0530

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,25 @@
+x=1
+while x<=10 :
+    print(x)
+    x+=1
+    
+    
+y=10
+while y>=1 :
+    print(y)
+    y-=1
+    
+x=1
+while x<5 :
+    print(x)
+    x+=1
+print(f"x={x}")    
+
+
+x=1
+is_bool=True
+while is_bool :
+    print(x)
+    x+=1
+    if x==5 :
+       is_bool=False
---
+++ nameprint2.py
@@ -0,0 +1,7 @@
+x=1
+count=True
+while count :
+    print("sarma")
+    x+=1
+    if x==11 :
+        count=False
